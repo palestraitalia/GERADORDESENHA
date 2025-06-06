dfa595898cbedf0207069c3688ce20c1a4704ab2
palestraitalia <141340393+palestraitalia@users.noreply.github.com>
2025-06-06 07:31:04 -0300

    .

---
+++ main.js
@@ -0,0 +1,6 @@
+<div class="parametro-senha-botoes">
+                        <button class="parametro-senha__botao">-</button>
+                        <p class="parametro-senha__texto">12</p>
+                        <button class="parametro-senha__botao">+</button>
+                    </div>
+                    
